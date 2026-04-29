dcd7709a16d2be78056d877a1a5d4f44d65b330b
KushagraCHAUDHARY <ch.kushagra1@gmail.com>
2026-04-29 11:36:36 +0530

    sorting

---
+++ sorting/selection.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] selectionSort(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n-1; i++){
+            int minindex = i;
+            for(int j=i; j<n; j++){
+                if(nums[j]<nums[minindex]){
+                    minindex = j;
+                }
+            }
+            int temp = nums[minindex];
+            nums[minindex] = nums[i];
+            nums[i] = temp;
+        }
+        return nums;
+    }
+}
